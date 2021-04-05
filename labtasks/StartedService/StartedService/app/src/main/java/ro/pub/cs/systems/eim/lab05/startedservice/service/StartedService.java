8aa7561c8ad8307312129e4fca4c4812645878b0
Andrei Sosea <sosea.andrei@yahoo.com>
2021-04-05 11:55:00 +0300

    Tasks 1-6

--- labtasks/StartedService/StartedService/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedservice/service/StartedService.java
+++ labtasks/StartedService/StartedService/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedservice/service/StartedService.java
@@ -1,6 +1,7 @@
 package ro.pub.cs.systems.eim.lab05.startedservice.service;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
@@ -38,10 +39,63 @@ public class StartedService extends Service {
         super.onDestroy();
     }
 
+    public class ProcessingThread extends Thread {
+
+        private Context context;
+
+        public ProcessingThread(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void run() {
+            Log.d(Constants.TAG, "Thread.run() was invoked, PID: " + android.os.Process.myPid() + " TID: " + android.os.Process.myTid());
+            while(true) {
+                sendMessage(Constants.MESSAGE_STRING);
+                sleep();
+                sendMessage(Constants.MESSAGE_INTEGER);
+                sleep();
+                sendMessage(Constants.MESSAGE_ARRAY_LIST);
+                sleep();
+            }
+        }
+
+        private void sendMessage(int messageType) {
+            Intent intent = new Intent();
+            switch (messageType) {
+                case Constants.MESSAGE_STRING:
+                        intent.setAction(Constants.ACTION_STRING);
+                    intent.putExtra(Constants.DATA, Constants.STRING_DATA);
+                    break;
+                case Constants.MESSAGE_INTEGER:
+                    intent.setAction(Constants.ACTION_INTEGER);
+                    intent.putExtra(Constants.DATA, Constants.INTEGER_DATA);
+                    break;
+                case Constants.MESSAGE_ARRAY_LIST:
+                    intent.setAction(Constants.ACTION_ARRAY_LIST);
+                    intent.putExtra(Constants.DATA, Constants.ARRAY_LIST_DATA);
+                    break;
+            }
+            context.sendBroadcast(intent);
+        }
+
+        private void sleep() {
+            try {
+                Thread.sleep(Constants.SLEEP_TIME);
+            } catch (InterruptedException interruptedException) {
+                Log.e(Constants.TAG, interruptedException.getMessage());
+                interruptedException.printStackTrace();
+            }
+        }
+
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(Constants.TAG, "onStartCommand() method was invoked");
         // TODO: exercise 5 - implement and start the ProcessingThread
+        ProcessingThread processingThread = new ProcessingThread(this);
+        processingThread.start();
         return START_REDELIVER_INTENT;
     }
 
